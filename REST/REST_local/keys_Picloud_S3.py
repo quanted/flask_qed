236bb30d38d8bccf7588044e186d7fe3ba40c33b
Jon Flash <funkswing@gmail.com>
2014-06-30 11:31:44 -0400

    Created submodule

---
+++ REST/REST_local/keys_Picloud_S3.py
@@ -0,0 +1,10 @@
+picloud_api_key='3355'
+picloud_api_secretkey='212ed160e3f416fdac8a3b71c90f3016722856b9'
+
+amazon_s3_key='AKIAJIHTMZIZEAQIAWBA'
+amazon_s3_secretkey='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'
+
+aws_access_key_id='AKIAIMJZHGDQQOQK4HXA'
+aws_secret_access_key='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'
+
+
