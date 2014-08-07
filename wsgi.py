40c57a2d0baaa2842ace68496e8b78e6842cab57
Jon Flash <funkswing@gmail.com>
2014-08-07 13:35:36 -0400

    WSGI Setup cont.

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 # Change working directory so relative paths (and template lookup) work again
-os.chdir(os.path.dirname(__file__))
+# os.chdir(os.path.dirname(__file__))
 
-import bottle
-import REST_EC2.bottle_ec2
+# import bottle
+# import REST_EC2.bottle_ec2
 # ... build or import your bottle application here ...
 # Do NOT use bottle.run() with mod_wsgi
 
 # application = bottle.default_app()
-application = REST_EC2.bottle_ec2.application
+# application = REST_EC2.bottle_ec2.application
