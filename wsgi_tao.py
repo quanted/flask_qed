677ae954864c87e4b8c8bbe0dfa2a52f1b408c9c
Jon Flash <funkswing@gmail.com>
2014-12-16 15:36:41 +0000

    Updated wsgi Entry point for EC2

---
+++ wsgi_tao.py
@@ -0,0 +1,16 @@
+import bottle, os, sys
+
+# Change working directory so relative paths (and template lookup) work again
+os.chdir(os.path.dirname(__file__))
+
+# Add Django project & parent directory to Python PATH
+sys.path.insert(0, '/home/ubuntu/ubertool_ecorest/REST_UBER')
+sys.path.insert(0, '/home/ubuntu/ubertool_ecorest')
+#sys.path.insert(0, '/var/www/ubertool')
+
+# ... build or import your bottle application here ...
+#import REST_UBER.bottle_ec2
+import REST_UBER.bottle_ec2
+# Do NOT use bottle.run() with mod_wsgi/uwsgi
+
+application = bottle.default_app()
