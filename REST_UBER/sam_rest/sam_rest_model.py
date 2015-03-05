8a7c2f8bb18091216303fd0f8ffa6ed3a6e7478d
Jon Flash <funkswing@gmail.com>
2015-03-05 16:07:22 -0500

    SAM Async Run

--- REST_UBER/sam_rest/sam_rest_model.py
+++ REST_UBER/sam_rest/sam_rest_model.py
@@ -28,7 +28,14 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
 
 
-def sam_read_input_file(sam_input_file_path):
+def convert_text_to_html(sam_input_file_path):
+    """
+    sam_input_file_path: String; Absolute path to SAM.inp for current run
+
+    returns: String; SAM.inp as HTML, where endline chars are replace with <br>
+
+    Converts SAM.inp from text file to HTML.
+    """
 
     html = "<br><b>SAM.inp created:</b><br>"
 
@@ -38,42 +45,16 @@ def sam_read_input_file(sam_input_file_path):
     return html
 
 
-# def s3_upload(temp_sam_run_path):
+def update_mongo(temp_sam_run_path, jid, run_type):
+    """
+    sam_input_file_path: String; Absolute path to SAM output temporary directory
+    jid: String; SAM run jid
+    run_type: String; SAM run type ('single', 'qaqc', or 'batch')
 
-#     sam_output_zip = os.path.join(temp_sam_run_path, "sam_output.zip")
-#     print sam_output_zip
-#     zf = zipfile.ZipFile(sam_output_zip, "w", zipfile.ZIP_DEFLATED)
-#     for dirname, subdirs, files in os.walk(temp_sam_run_path):
-#         zf.write(dirname)
-#         for filename in files:
-#             if filename != "sam_output.zip":
-#                 zf.write(os.path.join(dirname, filename))
-#     zf.close()
-    
-    # try:
-    #     # Connect to S3 bucket, create key and set value to zip file
-    #     conn = S3Connection(key, secretkey, is_secure=False)
-    #     bucket = Bucket(conn, 'super_przm')
-    #     k = Key(bucket)
-        
-    #     name1 = 'SAM_'+name_temp+'.zip'
-    #     k.key = name1
-    #     k.set_contents_from_filename(sam_output_zip)
-    #     link = 'https://s3.amazonaws.com/super_przm/' + name1
-    #     k.set_acl('public-read-write')
-    #     print link
+    Saves SAM output and SAM.inp to MongoDB.
+    """
 
-    #     return link
-
-    # except Exception, e:
-    #     print "Upload failed"
-    #     print "Error: ", str(e)
-
-    #     return False
-
-
-def update_mongo(run_type, jid):
-
+    logging.info("update_mongo() executed!")
     import pymongo
     client = pymongo.MongoClient('localhost', 27017)
     db = client.ubertool
@@ -83,12 +64,19 @@ def update_mongo(run_type, jid):
     """
     try:
         logging.info("Trying to open Eco_mth_avgdur.out file")
-        f = open(os.path.join(temp_sam_run_path, "EcoPestOut_all", "EcoPestOut_UpdatedGUI", "Test1", "Eco_mth_avgdur.out"), 'r')
-        output = f.read()
+        f_out = open(os.path.join(temp_sam_run_path, "EcoPestOut_all", "EcoPestOut_UpdatedGUI", "Test1", "Eco_mth_avgdur.out"), 'r')
+        logging.info("Eco_mth_avgdur.out opened!")
+        sam_output = f_out.read()
+
+        logging.info("Trying to open SAM.inp file")
+        f_input = open(os.path.join(temp_sam_run_path, "SAM.inp"), 'r')
+        logging.info("SAM.inp opened!")
+        sam_input = f_input.read()
 
     except Exception, e:
-        logging.info("Error: ", str(e))
-        output = "Error reading SAM output file"
+        logging.exception(e)
+        sam_output = "Error reading SAM output file"
+        sam_input = "Error reading SAM input file"
    
     document = {
         "user_id": "admin",
@@ -96,25 +84,53 @@ def update_mongo(run_type, jid):
         "run_type": run_type,
         "model_object_dict": {
             'filename': "Eco_mth_avgdur.out",
-            'output': output
+            'output': sam_output,
+            'input': sam_input
         }
     }
     db['sam'].save(document)
 
 
-def sam_callback(temp_sam_run_path, run_type, jid):
-    logging.info("sam_callback executed!")
-    # os.makedirs(os.path.join(temp_sam_run_path, 'callback'))
+def sam_callback(temp_sam_run_path, jid, run_type, future):
+    """
+    temp_sam_run_path: String; Absolute path to SAM output temporary directory
+    jid: String; SAM run jid
+    run_type: String; SAM run type ('single', 'qaqc', or 'batch')
+    future: concurrent.Future object; automatically passed in from concurrent.Future.add_done_callback()
 
-    # link = s3_upload(temp_sam_run_path, name_temp)
+    Callback function for when SuperPRZMPestide.exe has completed. 
+    Calls update_mongo() to insert output file into MongoDB. 
+    Deletes SAM output temporary directory.
+    """
+    logging.info("temp_sam_run_path = %s" %temp_sam_run_path)
+    logging.info("jid = %s" %jid)
+    logging.info("run_type = %s" %run_type)
 
-    # if link:
-    update_mongo(run_type, jid)
+    update_mongo(temp_sam_run_path, jid, run_type)
+
+    shutil.rmtree(temp_sam_run_path)
 
 
 def sam(inputs_json, jid, run_type):
     """
-        Custom or pre-canned run?
+    inputs_json; String (JSON); 
+    jid: String; SAM run jid
+    run_type: String; SAM run type ('single', 'qaqc', or 'batch')
+
+    SAM model run entry point.  Determines if run is to be pre-canned (demo) 
+    or an actual SuperPRZMpesticide.exe run.
+
+    If pre-canned:
+    returns: dict; Filled with dummy values.
+
+    If actual run:
+    returns: String (HTML); SAM.inp formatted in HTML.
+
+    Actual run launches SuperPRZMPesticide after determining which OS is being used 
+    in a separate process.  A Futures object is created and when the process is finished 
+    executing (SAM completes or is killed) a callback function is fired (sam_callback()).  
+    The callback function stores the output and input file into MongoDB for later 
+    retrieval.
     """
     args = json.loads(inputs_json)
 
@@ -122,6 +138,7 @@ def sam(inputs_json, jid, run_type):
     name_temp = id_generator()
     print name_temp
 
+    # Custom or pre-canned run?
     if args['scenario_selection'] == '0':
         logging.info('++++++++++++ C U S T O M ++++++++++++')
         # Run SAM, but first generate SAM input file        
@@ -145,24 +162,25 @@ def sam(inputs_json, jid, run_type):
                 print "Linux OS"
                 # Linux / UNIX based OS
                 exe = "SuperPRZMpesticide.exe"
+                
                 import subprocess32 as subprocess    # Use subprocess32 for Linux (Python 3.2 backport)
                 from concurrent.futures import ThreadPoolExecutor as Pool
-                
+                from functools import partial
+
+                # Create ThreadPoolExecutor (as 'Pool') instance to store threads which execute Fortran exe as subprocesses
+                pool = Pool(max_workers=1)
+
                 sam_path = os.path.join(curr_path, 'bin', 'ubertool_superprzm_src', 'Debug', exe)
                 print sam_path
-
                 # Define SuperPRZMpesticide.exe command line arguments
                 sam_arg1 = os.path.join(curr_path, 'bin')     # Absolute path to "root" of SAM model
                 sam_arg2 = name_temp                          # Temp directory name for SAM run
                 # Create list of args
                 args = sam_path + " " + sam_arg1 + " " + sam_arg2
 
-                # Create ThreadPoolExecutor (as 'Pool') instance to store threads which execute Fortran exe as subprocesses
-                pool = Pool(max_workers=1)
-                # Store execution of Fortran exe as a Future object, allowing the Python code to continue executing
-                future = pool.submit(subprocess.call, args, shell=1)
+                future = pool.submit(subprocess.call, args)
                 # When the Fortran exe finishes call "sam_callback" callback function
-                future.add_done_callback(sam_callback(temp_sam_run_path, run_type, jid))
+                future.add_done_callback(partial(sam_callback, temp_sam_run_path, jid, run_type))
                 # Destroy the Pool object which hosts the threads
                 pool.shutdown(wait=False)
 
@@ -170,28 +188,45 @@ def sam(inputs_json, jid, run_type):
                 print "Windows (really NOT Linux) OS"
                 # Assuming Windows here, could be other tho and this will break
                 exe = "SuperPRZMpesticide_win.exe"
-                # import subprocess
-                """
-                Don't actually run SAM, just delay 5 seconds
-                """
-                import time
-                time.sleep(5)
-                sam_callback(temp_sam_run_path, run_type, jid)
+                import subprocess
 
-            #try:
-            #    p = subprocess.Popen(args, shell=1)
-                
-            #except Exception, e:
-            #    logging.info("Error Msg: " + str(e))
-            #    pass
-            
-            input_file_string = sam_read_input_file(sam_input_file_path)
+                try:
+                    from concurrent.futures import ThreadPoolExecutor as Pool
+                    from functools import partial
 
-            return input_file_string
+                    # Create ThreadPoolExecutor (as 'Pool') instance to store threads which execute Fortran exe as subprocesses
+                    pool = Pool(max_workers=1)
+
+                    sam_path = os.path.join(curr_path, 'bin', 'ubertool_superprzm_src', 'Debug', exe)
+                    print sam_path
+                    # Define SuperPRZMpesticide.exe command line arguments
+                    sam_arg1 = os.path.join(curr_path, 'bin')     # Absolute path to "root" of SAM model
+                    sam_arg2 = name_temp                          # Temp directory name for SAM run
+                    # Create list of args
+                    args = sam_path + " " + sam_arg1 + " " + sam_arg2
+
+                    future = pool.submit(subprocess.call, args)
+                    # When the Fortran exe finishes call "sam_callback" callback function
+                    future.add_done_callback(partial(sam_callback, temp_sam_run_path, jid, run_type))
+                    # Destroy the Pool object which hosts the threads
+                    pool.shutdown(wait=False)
+
+                except Exception, e:
+                    logging.exception(e)
+
+                    """
+                    Don't actually run SAM, just delay a few seconds...
+                    """
+                    future = pool.submit(subprocess.Popen, "timeout 3")
+
+
+            input_file_html = convert_text_to_html(sam_input_file_path)
+
+            return input_file_html
 
 
         except Exception, e:
-            logging.info("Error Msg: " + str(e))
+            logging.exception(e)
             return {'user_id':'admin', 'result': {'error': str(e)}, '_id':jid}
     else:
         logging.info('++++++++++++ E L S E ++++++++++++')
