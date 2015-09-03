2269b1e9125f579d63498e9af87844c3942b4b62
puruckertom <purucker.tom@gmail.com>
2015-09-03 00:37:11 -0400

    sip process qaqc script

--- REST_UBER/sip_rest/sip_test.py
+++ REST_UBER/sip_rest/sip_test.py
@@ -3,29 +3,12 @@ import sip_model_rest as sip_model
 import pandas as pd
 import numpy.testing as npt
 
-csv_transpose_path_in = "./sip_qaqc_in_transpose.csv"
-csv_transpose_path_exp = "./sip_qaqc_exp_transpose.csv"
-
-# # needs to be run whenever the qaqc csv is updated
-# csv_path = "./sip_qaqc.csv"
-#
-# pd_obj_inputs = pd.read_csv(csv_path, index_col=0, header=None, skiprows=1, skipfooter=32, engine='python')
-# pd_obj_inputs = pd_obj_inputs.drop(labels=pd_obj_inputs.columns[range(4)], axis=1)
-# pd_obj_inputs.index.name = None
-# pd_obj_inputs.columns -= 5
-# pd_obj_inputs_transposed = pd_obj_inputs.transpose()
-# pd_obj_inputs_transposed.to_csv(csv_transpose_path_in)
-#
-# pd_obj_exp_out = pd.read_csv(csv_path, index_col=0, header=None, skiprows=33, engine='python')
-# pd_obj_exp_out = pd_obj_exp_out.drop(labels=pd_obj_exp_out.columns[range(4)], axis=1)
-# pd_obj_exp_out.index.name = None
-# pd_obj_exp_out.columns -= 5
-# pd_obj_exp_out_transposed = pd_obj_exp_out.transpose()
-# pd_obj_exp_out_transposed.to_csv(csv_transpose_path_exp)
 
 # load transposed qaqc data for inputs and expected outputs
+csv_transpose_path_in = "./sip_qaqc_in_transpose.csv"
 pd_obj_inputs = pd.read_csv(csv_transpose_path_in, index_col=0, engine='python')
 # print(pd_obj_inputs)
+csv_transpose_path_exp = "./sip_qaqc_exp_transpose.csv"
 pd_obj_exp_out = pd.read_csv(csv_transpose_path_exp, index_col=0, engine='python')
 # print(pd_obj_exp_out)
 
