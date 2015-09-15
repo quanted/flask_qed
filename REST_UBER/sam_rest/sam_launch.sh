1266cee414e47786f326852d07dd3e253f89c5e6
Jon Flash <funkswing@gmail.com>
2015-09-15 10:54:19 -0400

    [SAM] Testing multiprocessing script

--- REST_UBER/sam_rest/sam_launch.sh
+++ REST_UBER/sam_rest/sam_launch.sh
@@ -1,15 +1,18 @@
+#!/usr/bin/env bash
 #Activate the Python virtual environment
 source /var/www/ubertool/ubertool_ecorest/env/bin/activate
+echo python --version
 
 if [ $# -gt 0 ]; then
     echo '1st command line arg: ' $1
     echo '2nd command line arg: ' $2
     echo '3rd command line arg: ' $3
-    SAM_PY = $1
-    NAME_TEMP = $2
-    NUMBER_OF_ROWS_LIST = $3
+    SAM_PY=$1
+    NAME_TEMP=$2
+    NUMBER_OF_ROWS_LIST=$3
 
-    python SAM_PY NAME_TEMP NUMBER_OF_ROWS_LIST
+    sleep 15
+    #python $SAM_PY $NAME_TEMP "$NUMBER_OF_ROWS_LIST"
 else
     echo "Expected arguments missing"
 fi
