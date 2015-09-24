311dcf1667cb2110e677405a5f02f89492e5d0e2
kurtw555 <kurt_wolfe@yahoo.com>
2015-09-24 09:47:19 -0400

    Add rice test scripts

---
+++ REST_UBER/rice_rest/tests/test_rice_unittest.py
@@ -0,0 +1,77 @@
+import unittest
+import pandas as pd
+import numpy.testing as npt
+import pandas.util.testing as pdt
+from rice_rest import rice_model_rest as rice_model
+
+# create empty pandas dataframes to create empty sip object for testing
+df_empty = pd.DataFrame()
+# create an empty sip object
+rice_empty = rice_model.rice("empty", df_empty, df_empty)
+
+test = {}
+
+
+class TestRice(unittest.TestCase):
+    def setup(self):
+        pass
+        # sip2 = sip_model.sip(0, pd_obj_inputs, pd_obj_exp_out)
+        # setup the test as needed
+        # e.g. pandas to open sip qaqc csv
+        #  Read qaqc csv and create pandas DataFrames for inputs and expected outputs
+
+    def teardown(self):
+        pass
+        # teardown called after each test
+        # e.g. maybe write test results to some text file
+
+    #   dsed * area * pb
+    #   (self.dsed * self.area * self.pb)
+    def test_Calcmsed(self):
+        rice_empty.dsed = pd.Series([2.2], dtype='float')
+        rice_empty.area = pd.Series([3.3], dtype='float')
+        rice_empty.pb = pd.Series([4.4], dtype='float')
+
+        result = rice_empty.Calcmsed()
+        npt.assert_array_almost_equal(result, 31.944, 4, '', True)
+        return
+
+    # (self.dw * self.area) + (self.dsed * self.osed * self.area)
+    def test_Calcvw(self):
+        rice_empty.dw = pd.Series([2.2], dtype='float')
+        rice_empty.area = pd.Series([3.3], dtype='float')
+        rice_empty.dsed = pd.Series([4.4], dtype='float')
+        rice_empty.osed = pd.Series([5.5], dtype='float')
+
+        result = rice_empty.Calcvw()
+        npt.assert_array_almost_equal(result, 87.12, 4, '', True)
+        return
+
+    # (self.mai/self.area)*10000
+    def test_Calcmass_area(self):
+        rice_empty.area = pd.Series([1000.0], dtype='float')
+        rice_empty.mai = pd.Series([55.5.], dtype='float')
+        result = rice_empty.Calcmass_area()
+        npt.assert_array_almost_equal(result, 5.55., 4, '', True)
+        return
+
+    # (self.out_mass_area / (self.dw + (self.dsed * (self.osed + (self.pb * self.Kd*1e-5)))))*100
+    def test_Calccw(self):
+        rice_empty.dw = pd.Series([5.0], dtype='float')
+        rice_empty.dsed = pd.Series([4.0], dtype='float')
+        rice_empty.osed = pd.Series([3.0], dtype='float')
+        rice_empty.pb = pd.Series([2.0], dtype='float')
+        rice_empty.kd = pd.Series([100000.], dtype='float')
+        result = rice_empty.Calccw()
+        npt.assert_array_almost_equal(result, 25., 4, '', True)
+        return
+
+
+
+# unittest will
+# 1) call the setup method,
+# 2) then call every method starting with "test",
+# 3) then the teardown method
+if __name__ == '__main__':
+    unittest.main()
+    #pass
