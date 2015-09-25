c4aae82c7f0dfd9313e3de11eaa1fc514b87a8b4
Tom Purucker <purucker.tom@gmail.com>
2015-09-25 00:06:21 -0400

    tabular output for sip nosetests

--- REST_UBER/sip_rest/tests/test_sip_integration.py
+++ REST_UBER/sip_rest/tests/test_sip_integration.py
@@ -1,10 +1,13 @@
-import unittest
-import pandas as pd
-import numpy.testing as npt
 import pkgutil
+import unittest
 from StringIO import StringIO
-from .. import sip_model_rest as sip_model
+from tabulate import tabulate
+
+import numpy.testing as npt
+import pandas as pd
 import pandas.util.testing as pdt
+
+from .. import sip_model_rest as sip_model
 
 # load transposed qaqc data for inputs
 #works for local nosetests from parent directory
@@ -14,7 +17,11 @@ import pandas.util.testing as pdt
 #this works for both local nosetests and travis deploy
 data_inputs = StringIO(pkgutil.get_data(__package__, 'sip_qaqc_in_transpose.csv'))
 pd_obj_inputs = pd.read_csv(data_inputs, index_col=0, engine='python')
-print(pd_obj_inputs)
+print("sip inputs")
+print(pd_obj_inputs.shape)
+print(tabulate(pd_obj_inputs.iloc[:,0:5], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_inputs.iloc[:,6:10], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_inputs.iloc[:,11:16], headers='keys', tablefmt='fancy_grid'))
 
 # load transposed qaqc data for inputs
 #works for local nosetests, but not for travis container that calls nosetests:
@@ -23,7 +30,12 @@ print(pd_obj_inputs)
 #this works for both local nosetests and travis deploy
 data_exp_outputs = StringIO(pkgutil.get_data(__package__, 'sip_qaqc_exp_transpose.csv'))
 pd_obj_exp_out = pd.read_csv(data_exp_outputs, index_col=0, engine='python')
-print(pd_obj_exp_out)
+print("sip expected outputs")
+print(pd_obj_exp_out.shape)
+print(tabulate(pd_obj_exp_out.iloc[:,0:5], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_exp_out.iloc[:,6:10], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_exp_out.iloc[:,11:13], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_exp_out.iloc[:,14:16], headers='keys', tablefmt='fancy_grid'))
 
 # create an instance of sip object with qaqc data
 sip_calc = sip_model.sip("batch", pd_obj_inputs, pd_obj_exp_out)
@@ -31,6 +43,12 @@ test = {}
 
 
 class TestSip(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        #adding to TestCase constructor so super
+        super(TestSip, self).__init__(*args, **kwargs)
+        self.ncases = len(pd_obj_inputs)
+
     def setup(self):
         pass
         # sip2 = sip_model.sip(0, pd_obj_inputs, pd_obj_exp_out)
@@ -45,50 +63,155 @@ class TestSip(unittest.TestCase):
 
 #Note: commented-out rows contain output files that are not running properly in the subsequent blackbox_method test.
     def integration_test_dose_bird(self):
-         self.blackbox_method_int('dose_bird')
+        '''
+        integration test for output sip.dose_bird
+        '''
+        try:
+            self.blackbox_method_int('dose_bird')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_dose_mamm(self):
-         self.blackbox_method_int('dose_mamm')
+    def integration_test_dose_mamm(self):
+        '''
+        integration test for output sip.dose_mamm
+        '''
+        try:
+            self.blackbox_method_int('dose_mamm')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_at_bird(self):
-         self.blackbox_method_int('at_bird')
+    def integration_test_at_bird(self):
+        '''
+        integration test for output sip.at_bird
+        '''
+        try:
+            self.blackbox_method_int('at_bird')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_at_mamm(self):
-         self.blackbox_method_int('at_mamm')
+    def integration_test_at_mamm(self):
+        '''
+        integration test for output sip.at_mamm
+        '''
+        try:
+            self.blackbox_method_int('at_mamm')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_fi_bird(self):
-        #self.blackbox_method('fi_bird')
-        pass
+    def integration_test_fi_bird(self):
+        '''
+        integration test for output sip.fi_bird
+        '''
+        try:
+            #self.blackbox_method('fi_bird')
+            pass
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_det(self):
-         self.blackbox_method_int('det')
+    def integration_test_det(self):
+        '''
+        integration test for output sip.det
+        '''
+        try:
+            self.blackbox_method_int('det')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_act(self):
-         self.blackbox_method_int('act')
+    def integration_test_act(self):
+        '''
+        integration test for output sip.act
+        '''
+        try:
+            self.blackbox_method_int('act')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_acute_bird(self):
-         self.blackbox_method_int('acute_bird')
+    def integration_test_acute_bird(self):
+        '''
+        integration test for output sip.acute_bird
+        '''
+        try:
+            self.blackbox_method_int('acute_bird')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_acuconb(self):
-         self.blackbox_method_str('acuconb')
+    def integration_test_acuconb(self):
+        '''
+        integration test for output sip.acuconb
+        '''
+        try:
+            self.blackbox_method_str('acuconb')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_acute_mamm(self):
-         self.blackbox_method_int('acute_mamm')
+    def integration_test_acute_mamm(self):
+        '''
+        integration test for output sip.acute_mamm
+        '''
+        try:
+            self.blackbox_method_int('acute_mamm')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_acuconm(self):
-         self.blackbox_method_str('acuconm')
+    def integration_test_acuconm(self):
+        '''
+        integration test for output sip.acuconm
+        '''
+        try:
+            self.blackbox_method_str('acuconm')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_chron_bird(self):
-         self.blackbox_method_int('chron_bird')
+    def integration_test_chron_bird(self):
+        '''
+        integration test for output sip.chron_bird
+        '''
+        try:
+            self.blackbox_method_int('chron_bird')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_chronconb(self):
-         self.blackbox_method_str('chronconb')
+    def integration_test_chronconb(self):
+        '''
+        integration test for output sip.chronconb
+        '''
+        try:
+            self.blackbox_method_str('chronconb')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_chron_mamm(self):
-         self.blackbox_method_int('chron_mamm')
+    def integration_test_chron_mamm(self):
+        '''
+        integration test for output sip.chron_mamm
+        '''
+        try:
+            self.blackbox_method_int('chron_mamm')
+        finally:
+            pass
+        return
 
-    def integration_test_blackbox_chronconm(self):
-         self.blackbox_method_str('chronconm')
+    def integration_test_chronconm(self):
+        '''
+        integration test for output sip.chronconm
+        '''
+        try:
+            self.blackbox_method_str('chronconm')
+        finally:
+            pass
+        return
 
     def blackbox_method_int(self, output):
         """
@@ -98,11 +221,17 @@ class TestSip(unittest.TestCase):
         """
         result = sip_calc.pd_obj_out[output + "_out"]
         expected = sip_calc.pd_obj_exp[output + "_exp"]
+        tab = pd.concat([result, expected], axis=1)
+        print(" ")
+        print(tabulate(tab, headers='keys', tablefmt='fancy_grid'))
         npt.assert_array_almost_equal(result, expected, 4, '', True)
 
     def blackbox_method_str(self, output):
         result = sip_calc.pd_obj_out[output + "_out"]
         expected = sip_calc.pd_obj_exp[output + "_exp"]
+        tab = pd.concat([result, expected], axis=1)
+        print(" ")
+        print(tabulate(tab, headers='keys', tablefmt='fancy_grid'))
         npt.assert_array_equal(result, expected)
 
 # unittest will
--- REST_UBER/sip_rest/tests/test_sip_unittest.py
+++ REST_UBER/sip_rest/tests/test_sip_unittest.py
@@ -1,11 +1,9 @@
 import unittest
-import pandas as pd
+
 import numpy.testing as npt
+import pandas as pd
 import pandas.util.testing as pdt
-#following works when running test script in parent directory as package:
-# python -m tests.test_sip_unittest
-#import sip_model_rest as sip_model
-#following works for running as nosetests from parent directory:
+
 from .. import sip_model_rest as sip_model
 
 # create empty pandas dataframes to create empty sip object for testing
@@ -28,72 +26,124 @@ class TestSip(unittest.TestCase):
         # teardown called after each test
         # e.g. maybe write test results to some text file
 
-    def unittest_fw_bird(self):
-        result = sip_empty.fw_bird()
-        npt.assert_array_almost_equal(result, 0.0162, 4, '', True)
+    def unit_test_fw_bird(self):
+        '''
+        unittest for function sip.fw_bird:
+        '''
+        try:
+            result = sip_empty.fw_bird()
+            npt.assert_array_almost_equal(result, 0.0162, 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_fw_mamm(self):
-        result = sip_empty.fw_mamm()
-        npt.assert_array_almost_equal(result, 0.172, 4, '', True)
+    def unit_test_fw_mamm(self):
+        '''
+        unittest for function sip.fw_mamm:
+        '''
+        try:
+            result = sip_empty.fw_mamm()
+            npt.assert_array_almost_equal(result, 0.172, 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_dose_bird(self):
-        #(self.fw_bird_out * self.solubility)/(self.bodyweight_assessed_bird / 1000.)
-        sip_empty.fw_bird_out = pd.Series([10.], dtype='int')
-        sip_empty.solubility = pd.Series([100.], dtype='int')
-        sip_empty.bodyweight_assessed_bird = pd.Series([1.], dtype='int')
-        result = sip_empty.dose_bird()
-        npt.assert_array_almost_equal(result, 1000000., 4, '', True)
+    def unit_test_dose_bird(self):
+        '''
+        unittest for function sip.dose_bird:
+        '''
+        try:
+            #(self.fw_bird_out * self.solubility)/(self.bodyweight_assessed_bird / 1000.)
+            sip_empty.fw_bird_out = pd.Series([10.], dtype='int')
+            sip_empty.solubility = pd.Series([100.], dtype='int')
+            sip_empty.bodyweight_assessed_bird = pd.Series([1.], dtype='int')
+            result = sip_empty.dose_bird()
+            npt.assert_array_almost_equal(result, 1000000., 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_dose_mamm(self):
-        #(self.fw_mamm_out * self.solubility)/(self.bodyweight_assessed_mammal / 1000)
-        sip_empty.fw_mamm_out = pd.Series([20.], dtype='int')
-        sip_empty.solubility = pd.Series([400.], dtype='int')
-        sip_empty.bodyweight_assessed_mammal = pd.Series([1.], dtype='int')
-        result = sip_empty.dose_mamm()
-        npt.assert_array_almost_equal(result, 8000000., 4, '', True)
+    def unit_test_dose_mamm(self):
+        '''
+        unittest for function sip.dose_mamm:
+        '''
+        try:
+            #(self.fw_mamm_out * self.solubility)/(self.bodyweight_assessed_mammal / 1000)
+            sip_empty.fw_mamm_out = pd.Series([20.], dtype='int')
+            sip_empty.solubility = pd.Series([400.], dtype='int')
+            sip_empty.bodyweight_assessed_mammal = pd.Series([1.], dtype='int')
+            result = sip_empty.dose_mamm()
+            npt.assert_array_almost_equal(result, 8000000., 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_at_bird(self):
-        #(self.ld50_avian_water) * ((self.bodyweight_assessed_bird / self.bodyweight_tested_bird)**(self.mineau_scaling_factor - 1.))
-        sip_empty.ld50_avian_water = pd.Series([2000.], dtype='int')
-        sip_empty.bodyweight_assessed_bird = pd.Series([100.], dtype='int')
-        sip_empty.bodyweight_tested_bird = pd.Series([200.], dtype='int')
-        sip_empty.mineau_scaling_factor = pd.Series([2.], dtype='int')
-        result = sip_empty.at_bird()
-        npt.assert_array_almost_equal(result, 1000., 4, '', True)
+    def unit_test_at_bird(self):
+        '''
+        unittest for function sip.at_bird:
+        '''
+        try:
+            #(self.ld50_avian_water) * ((self.bodyweight_assessed_bird / self.bodyweight_tested_bird)**(self.mineau_scaling_factor - 1.))
+            sip_empty.ld50_avian_water = pd.Series([2000.], dtype='int')
+            sip_empty.bodyweight_assessed_bird = pd.Series([100.], dtype='int')
+            sip_empty.bodyweight_tested_bird = pd.Series([200.], dtype='int')
+            sip_empty.mineau_scaling_factor = pd.Series([2.], dtype='int')
+            result = sip_empty.at_bird()
+            npt.assert_array_almost_equal(result, 1000., 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_at_mamm(self):
-        #(self.ld50_mammal_water) * ((self.bodyweight_tested_mammal / self.bodyweight_assessed_mammal)**0.25)
-        sip_empty.ld50_mammal_water = pd.Series([10.], dtype='int')
-        sip_empty.bodyweight_tested_mammal = pd.Series([100.], dtype='int')
-        sip_empty.bodyweight_assessed_mammal = pd.Series([200.], dtype='int')
-        result = sip_empty.at_mamm()
-        npt.assert_array_almost_equal(result, 8.408964, 4, '', True)
+    def unit_test_at_mamm(self):
+        '''
+        unittest for function sip.at_mamm:
+        '''
+        try:
+            #(self.ld50_mammal_water) * ((self.bodyweight_tested_mammal / self.bodyweight_assessed_mammal)**0.25)
+            sip_empty.ld50_mammal_water = pd.Series([10.], dtype='int')
+            sip_empty.bodyweight_tested_mammal = pd.Series([100.], dtype='int')
+            sip_empty.bodyweight_assessed_mammal = pd.Series([200.], dtype='int')
+            result = sip_empty.at_mamm()
+            npt.assert_array_almost_equal(result, 8.408964, 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_fi_bird(self):
-        #0.0582 * ((bw_grams / 1000.)**0.651)
-        sip_empty.bw_grams = pd.Series([100.], dtype='int')
-        result = sip_empty.fi_bird(sip_empty.bw_grams)
-        npt.assert_array_almost_equal(result, 0.012999, 4, '', True)
+    def unit_test_fi_bird(self):
+        '''
+        unittest for function sip.fi_bird:
+        '''
+        try:
+            #0.0582 * ((bw_grams / 1000.)**0.651)
+            sip_empty.bw_grams = pd.Series([100.], dtype='int')
+            result = sip_empty.fi_bird(sip_empty.bw_grams)
+            npt.assert_array_almost_equal(result, 0.012999, 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_act(self):
-        #(self.noael_mammal_water) * ((self.bodyweight_tested_mammal / self.bodyweight_assessed_mammal)**0.25)
-        sip_empty.noael_mammal_water = pd.Series([10.], dtype='int')
-        sip_empty.bodyweight_tested_mammal = pd.Series([500.], dtype='int')
-        sip_empty.bodyweight_assessed_mammal = pd.Series([400.], dtype='int')
-        result = sip_empty.act()
-        npt.assert_array_almost_equal(result, 10.5737, 4, '', True)
+    def unit_test_act(self):
+        '''
+        unittest for function sip.test_act:
+        '''
+        try:
+            #(self.noael_mammal_water) * ((self.bodyweight_tested_mammal / self.bodyweight_assessed_mammal)**0.25)
+            sip_empty.noael_mammal_water = pd.Series([10.], dtype='int')
+            sip_empty.bodyweight_tested_mammal = pd.Series([500.], dtype='int')
+            sip_empty.bodyweight_assessed_mammal = pd.Series([400.], dtype='int')
+            result = sip_empty.act()
+            npt.assert_array_almost_equal(result, 10.5737, 4, '', True)
+        finally:
+            pass
         return
 
 #Weird equation. Let's talk about this one.
-    def unittest_det(self):
-        """
+    def unit_test_det(self):
+        '''
+        unittest for function sip.det:
+        '''
+
+        '''
         Dose Equiv. Toxicity:
 
         The FI value (kg-diet) is multiplied by the reported NOAEC (mg/kg-diet) and then divided by
@@ -105,48 +155,77 @@ class TestSip(unittest.TestCase):
         and for any other test species. The model calculates the dose equivalent toxicity values for
         all of the modeled values (Cells F20-24 and results worksheet) and then selects the lowest dose
         equivalent toxicity value to represent the chronic toxicity of the chemical to birds.
-        """
-        # result =
-        # self.assertEquals(result, )
-        # return
-
-#Amber working
-    # def test_det_duck(self):
-    # # det_duck = (self.noaec_duck * self.fi_bird(1580.)) / (1580. / 1000.)
-    #     sip_empty.noaec_duck = pd.Series([1.], dtype='int')
-    #     sip_empty.fi_bird = pd.Series([1.], dtype='int')
-    #     result = sip_empty.det_duck()
-    #     npt.assert_array_almost_equal(result, 1000., 4, '', True)
-
-#Amber working
-    # def test_det_quail(self):
-    # # det_quail = (self.noaec_quail * self.fi_bird(178.)) / (178. / 1000.)
-    #     sip_empty.noaec_quail = pd.Series([1.], dtype='int')
-    #     sip_empty.fi_bird = pd.Series([1.], dtype='int')
-    #     result = sip_empty.det_quail()
-    #     npt.assert_array_almost_equal(result, 1000., 4, '', True)
-
-#Carmen
-#        det_other_1 = (self.noaec_bird_other_1 * self.fi_bird(self.bodyweight_bird_other_1)) / (self.bodyweight_bird_other_1 / 1000.)
-#        det_other_2 = (self.noaec_bird_other_2 * self.fi_bird(self.bodyweight_bird_other_1)) / (self.bodyweight_bird_other_1 / 1000.)
-
-#Carmen
-    # def test_det_other_1(self):
-    #     sip_empty.noaec_bird_other_1 = pd.Series([400.]) # mg/kg-diet
-    #     sip_empty.bodyweight_bird_other_1 = pd.Series([100]) # grams
-    #     result = sip_empty.det_other_1()
-    #     npt.assert_array_almost_equal(result, 4666, 4)
-    #     return
-
-    def unittest_acute_bird(self):
-        # self.acute_bird_out = self.dose_bird_out / self.at_bird_out
-        sip_empty.dose_bird_out = pd.Series([100.], dtype='int')
-        sip_empty.at_bird_out = pd.Series([10.], dtype='int')
-        result = sip_empty.acute_bird()
-        npt.assert_array_almost_equal(result, 10., 4)
+        '''
+        try:
+            # result =
+            # self.assertEquals(result, )
+            pass
+        finally:
+            pass
         return
 
-    def unittest_acuconb(self):
+
+    def test_det_duck(self):
+        '''
+        unittest for function sip.det_duck:
+        '''
+        try:
+            # det_duck = (self.noaec_duck * self.fi_bird(1580.)) / (1580. / 1000.)
+            sip_empty.noaec_duck = pd.Series([1.], dtype='int')
+            sip_empty.fi_bird = pd.Series([1.], dtype='int')
+            result = sip_empty.det_duck()
+            npt.assert_array_almost_equal(result, 1000., 4, '', True)
+        finally:
+            pass
+        return
+
+    def test_det_quail(self):
+        '''
+        unittest for function sip.det_quail:
+        '''
+        try:
+            # det_quail = (self.noaec_quail * self.fi_bird(178.)) / (178. / 1000.)
+            sip_empty.noaec_quail = pd.Series([1.], dtype='int')
+            sip_empty.fi_bird = pd.Series([1.], dtype='int')
+            result = sip_empty.det_quail()
+            npt.assert_array_almost_equal(result, 1000., 4, '', True)
+        finally:
+            pass
+        return
+
+    def test_det_other_1(self):
+        '''
+        unittest for function sip.det_other_1:
+        '''
+        try:
+            #det_other_1 = (self.noaec_bird_other_1 * self.fi_bird(self.bodyweight_bird_other_1)) / (self.bodyweight_bird_other_1 / 1000.)
+            #det_other_2 = (self.noaec_bird_other_2 * self.fi_bird(self.bodyweight_bird_other_1)) / (self.bodyweight_bird_other_1 / 1000.)
+            sip_empty.noaec_bird_other_1 = pd.Series([400.]) # mg/kg-diet
+            sip_empty.bodyweight_bird_other_1 = pd.Series([100]) # grams
+            result = sip_empty.det_other_1()
+            npt.assert_array_almost_equal(result, 4666, 4)
+        finally:
+            pass
+        return
+
+    def unit_test_acute_bird(self):
+        '''
+        unittest for function sip.acute_bird:
+        '''
+        try:
+            # self.acute_bird_out = self.dose_bird_out / self.at_bird_out
+            sip_empty.dose_bird_out = pd.Series([100.], dtype='int')
+            sip_empty.at_bird_out = pd.Series([10.], dtype='int')
+            result = sip_empty.acute_bird()
+            npt.assert_array_almost_equal(result, 10., 4)
+        finally:
+            pass
+        return
+
+    def unit_test_acuconb(self):
+        '''
+        unittest for function sip.acuconb:
+        '''
         """
         Message stating whether or not a risk is present
         """
@@ -154,25 +233,37 @@ class TestSip(unittest.TestCase):
         #     if self.acute_bird_out == None:
         #         raise ValueError\
         #         ('acute_bird variable equals None and therefor this function cannot be run.')
-            # if self.acute_bird_out < 0.1:
-            #     self.acuconb_out = ('Drinking water exposure alone is NOT a potential concern for birds')
-            # else:
-            #     self.acuconb_out = ('Exposure through drinking water alone is a potential concern for birds')
-        sip_empty.acute_bird_out = pd.Series([0.2])
-        result = sip_empty.acuconb()
-        exp = pd.Series(["Exposure through drinking water alone is a potential concern for birds"])
-        pdt.assert_series_equal(result, exp)
+        # if self.acute_bird_out < 0.1:
+        #     self.acuconb_out = ('Drinking water exposure alone is NOT a potential concern for birds')
+        # else:
+        #     self.acuconb_out = ('Exposure through drinking water alone is a potential concern for birds')
+        try:
+            sip_empty.acute_bird_out = pd.Series([0.2])
+            result = sip_empty.acuconb()
+            exp = pd.Series(["Exposure through drinking water alone is a potential concern for birds"])
+            pdt.assert_series_equal(result, exp)
+        finally:
+            pass
         return
 
-    def unittest_acute_mamm(self):
+    def unit_test_acute_mamm(self):
+        '''
+        unittest for function sip.acute_mamm:
+        '''
         # self.acute_mamm_out = self.dose_mamm_out / self.at_mamm_out
-        sip_empty.dose_mamm_out = pd.Series([100.], dtype='int')
-        sip_empty.at_mamm_out = pd.Series([10.], dtype='int')
-        result = sip_empty.acute_mamm()
-        npt.assert_array_almost_equal(result, 10., 4)
+        try:
+            sip_empty.dose_mamm_out = pd.Series([100.], dtype='int')
+            sip_empty.at_mamm_out = pd.Series([10.], dtype='int')
+            result = sip_empty.acute_mamm()
+            npt.assert_array_almost_equal(result, 10., 4)
+        finally:
+            pass
         return
 
-    def unittest_acuconm(self):
+    def unit_test_acuconm(self):
+        '''
+        unittest for function sip.acuconm:
+        '''
         """
         Message stating whether or not a risk is present
         """
@@ -185,47 +276,71 @@ class TestSip(unittest.TestCase):
         #     else:
         #         self.acuconm_out = ('Exposure through drinking water alone is a potential concern for mammals')
         #     return self.acuconm_out
-        sip_empty.acute_mamm_out = pd.Series([0.2])
-        result = sip_empty.acuconm()
-        exp = pd.Series(["Exposure through drinking water alone is a potential concern for mammals"])
-        pdt.assert_series_equal(result, exp)
+        try:
+            sip_empty.acute_mamm_out = pd.Series([0.2])
+            result = sip_empty.acuconm()
+            exp = pd.Series(["Exposure through drinking water alone is a potential concern for mammals"])
+            pdt.assert_series_equal(result, exp)
+        finally:
+            pass
         return
 
-    def unittest_chron_bird(self):
-    #self.chron_bird_out = self.dose_bird_out / self.det_out
-        sip_empty.dose_bird_out = pd.Series([5.], dtype='int')
-        sip_empty.det_out = pd.Series([10.], dtype='int')
-        result = sip_empty.chron_bird()
-        npt.assert_array_almost_equal(result, 0.5, 4, '', True)
+    def unit_test_chron_bird(self):
+        '''
+        unittest for function sip.chron_bird:
+        '''
+        #self.chron_bird_out = self.dose_bird_out / self.det_out
+        try:
+            sip_empty.dose_bird_out = pd.Series([5.], dtype='int')
+            sip_empty.det_out = pd.Series([10.], dtype='int')
+            result = sip_empty.chron_bird()
+            npt.assert_array_almost_equal(result, 0.5, 4, '', True)
+        finally:
+            pass
         return
 
-    def unittest_chronconb(self):
-    #boolean = self.chron_bird_out < 1
+    def unit_test_chronconb(self):
+        '''
+        unittest for function sip.chronconb:
+        '''
+        #boolean = self.chron_bird_out < 1
         #self.chronconb_out = boolean.map(lambda x:
-         #   'Drinking water exposure alone is NOT a potential concern for birds' if x == True
-         #   else 'Exposure through drinking water alone is a potential concern for birds')
-        sip_empty.chron_bird_out = pd.Series([3])
-        result = sip_empty.chronconb()
-        exp = pd.Series(["Exposure through drinking water alone is a potential concern for birds"])
-        pdt.assert_series_equal(result, exp)
+        #   'Drinking water exposure alone is NOT a potential concern for birds' if x == True
+        #   else 'Exposure through drinking water alone is a potential concern for birds')
+        try:
+            sip_empty.chron_bird_out = pd.Series([3])
+            result = sip_empty.chronconb()
+            exp = pd.Series(["Exposure through drinking water alone is a potential concern for birds"])
+            pdt.assert_series_equal(result, exp)
+        finally:
+            pass
         return
 
-    def unittest_chron_mamm(self):
-         # self.chron_mamm_out = self.dose_mamm_out / self.act_out
+    def unit_test_chron_mamm(self):
+        '''
+        unittest for function sip.chron_mamm:
+        '''
+        # self.chron_mamm_out = self.dose_mamm_out / self.act_out
         sip_empty.dose_mamm_out = pd.Series([8.], dtype='int')
         sip_empty.act_out = pd.Series([4.], dtype='int')
         result = sip_empty.chron_mamm()
         npt.assert_array_almost_equal(result, 2, 4, '', True)
         return
 
-    def unittest_chronconm(self):
+    def unit_test_chronconm(self):
+        '''
+        unittest for function sip.chronconm:
+        '''
         # self.chronconm_out = boolean.map(lambda x:
-         #   'Drinking water exposure alone is NOT a potential concern for mammals' if x == True
-         #   else 'Exposure through drinking water alone is a potential concern for mammals')
-        sip_empty.chron_mamm_out =  pd.Series([0.5])
-        result = sip_empty.chronconm()
-        exp = pd.Series(["Drinking water exposure alone is NOT a potential concern for mammals"])
-        pdt.assert_series_equal(result, exp)
+        #   'Drinking water exposure alone is NOT a potential concern for mammals' if x == True
+        #   else 'Exposure through drinking water alone is a potential concern for mammals')
+        try:
+            sip_empty.chron_mamm_out =  pd.Series([0.5])
+            result = sip_empty.chronconm()
+            exp = pd.Series(["Drinking water exposure alone is NOT a potential concern for mammals"])
+            pdt.assert_series_equal(result, exp)
+        finally:
+            pass
         return
 
 # unittest will
