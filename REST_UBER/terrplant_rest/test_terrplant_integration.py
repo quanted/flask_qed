511005e02a96cd75a40f50563b0fcdf4eae534ce
Carmen Kuan <ashleyc.kuan@gmail.com>
2015-09-28 16:01:25 -0400

    terrplant nosetests

---
+++ REST_UBER/terrplant_rest/test_terrplant_integration.py
@@ -0,0 +1,236 @@
+import unittest
+import terrplant_model_rest as terrplant_model
+import pandas as pd
+import numpy.testing as npt
+import pkgutil
+from StringIO import StringIO
+from .. import terrplant_model_rest as terrplant_model
+from tabulate import tabulate
+
+# load transposed qaqc data for inputs and expected outputs
+# works for local nosetests from parent directory
+# but not for travis container that calls nosetests:
+# csv_transpose_path_in = "./terrplant_qaqc_in_transpose.csv"
+# pd_obj_inputs = pd.read_csv(csv_transpose_path_in, index_col=0, engine='python')
+# print(pd_obj_inputs)
+# this works for both local nosetests and travis deploy
+data_inputs = StringIO(pkgutil.get_data(__package__, 'terrplant_qaqc_in_transpose.csv'))
+pd_obj_inputs = pd.read_csv(data_inputs, index_col=0, engine='python')
+print("terrplant inputs")
+print(pd_obj_inputs.shape)
+print(tabulate(pd_obj_inputs.iloc[:,0:5], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_inputs.iloc[:,6:10], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_inputs.iloc[:,11:13], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_inputs.iloc[:,14:17], headers='keys', tablefmt='fancy_grid'))
+
+# load transposed qaqc data for expected outputs
+# works for local nosetests from parent directory
+# but not for travis container that calls nosetests:
+# csv_transpose_path_exp = "./terrplant_qaqc_exp_transpose.csv"
+# pd_obj_exp_out = pd.read_csv(csv_transpose_path_exp, index_col=0, engine='python')
+# print(pd_obj_exp_out)
+# this works for both local nosetests and travis deploy
+data_exp_outputs = StringIO(pkgutil.get_data(__package__, 'terrplant_qaqc_exp_transpose.csv'))
+pd_obj_exp = pd.read_csv(data_exp_outputs, index_col=0, engine= 'python')
+print("terrplant expected outputs")
+print(pd_obj_exp.shape)
+print(tabulate(pd_obj_exp.iloc[:,0:5], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_exp.iloc[:,6:10], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_exp.iloc[:,11:14], headers='keys', tablefmt='fancy_grid'))
+print(tabulate(pd_obj_exp.iloc[:,15:16], headers='keys', tablefmt='fancy_grid'))
+
+# create an instance of sip object with qaqc data
+terrplant_calc = terrplant_model.terrplant("batch", pd_obj_inputs, pd_obj_exp)
+test = {}
+
+
+class TestTerrplant(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_rundry(self):
+        """
+        Integration test for terrplant.rundry
+        """
+        try:
+            self.blackbox_method_int('rundry')
+        finally:
+            pass
+        return
+
+    def test_runsemi(self):
+        """
+        Integration test for terrplant.runsemi
+        """
+        try:
+            self.blackbox_method_int('runsemi')
+        finally:
+            pass
+        return
+
+    def test_spray(self):
+        """
+        Integration test for terrplant.spray
+        """
+        try:
+            self.blackbox_method_int('spray')
+        finally:
+            pass
+        return
+    def test_totaldry(self):
+        """
+        Integration test for terrplant.totaldry
+        """
+        try:
+            self.blackbox_method_int('totaldry')
+        finally:
+            pass
+        return
+    def test_totalsemi(self):
+        """
+        Integration test for terrplant.totalsemi
+        """
+        try:
+            self.blackbox_method_int('totalsemi')
+        finally:
+            pass
+        return
+    def test_nms_rq_dry(self):
+        """
+        Integration test for terrplant.nms_rq_dry
+        """
+        try:
+            self.blackbox_method_int('nms_rq_dry')
+        finally:
+            pass
+        return
+    def test_nms_rq_semi(self):
+        """
+        Integration test for terrplant.nms_rq_semi
+        """
+        try:
+            self.blackbox_method_int('nms_rq_semi')
+        finally:
+            pass
+        return
+    def test_nms_rq_spray(self):
+        """
+        Integration test for terrplant.nms_rq_spray
+        """
+        try:
+            self.blackbox_method_int('nms_rq_spray')
+        finally:
+            pass
+        return
+    def test_lms_rq_dry(self):
+        """
+        Integration test for terrplant.lms_rq_dry
+        """
+        try:
+            self.blackbox_method_int('lms_rq_dry')
+        finally:
+            pass
+        return
+    def test_lms_rq_semi(self):
+        """
+        Integration test for terrplant.lms_rq_semi
+        """
+        try:
+            self.blackbox_method_int('lms_rq_semi')
+        finally:
+            pass
+        return
+    def test_lms_rq_spray(self):
+        """
+        Integration test for terrplant.lms_rq_spray
+        """
+        try:
+            self.blackbox_method_int('lms_rq_spray')
+        finally:
+            pass
+        return
+    def test_nds_rq_dry(self):
+        """
+        Integration test for terrplant.nds_rq_dry
+        """
+        try:
+            self.blackbox_method_int('nds_rq_dry')
+        finally:
+            pass
+        return
+    def test_nds_rq_semi(self):
+        """
+        Integration test for terrplant.nds_rq_semi
+        """
+        try:
+            self.blackbox_method_int('nds_rq_semi')
+        finally:
+            pass
+        return
+    def test_nds_rq_spray(self):
+        """
+        Integration test for terrplant.nds_rq_spray
+        """
+        try:
+            self.blackbox_method_int('nds_rq_spray')
+        finally:
+            pass
+        return
+    def test_lds_rq_dry(self):
+        """
+        Integration test for terrplant.lds_rq_dry
+        """
+        try:
+            self.blackbox_method_int('lds_rq_dry')
+        finally:
+            pass
+        return
+    def test_lds_rq_semi(self):
+        """
+        Integration test for terrplant.lds_rq_semi
+        """
+        try:
+            self.blackbox_method_int('lds_rq_semi')
+        finally:
+            pass
+        return
+    def test_lds_rq_spray(self):
+        """
+        Integration test for terrplant.lds_rq_spray
+        """
+        try:
+            self.blackbox_method_int('lds_rq_spray')
+        finally:
+            pass
+        return
+
+    def blackbox_method_int(self, output):
+        """
+        Helper method to reuse code for testing numpy array outputs from TerrPlant model
+        :param output: String; Pandas Series name (e.g. column name) without '_out'
+        :return:
+        """
+        result = terrplant_calc.pd_obj_out["out_" + output]
+        expected = terrplant_calc.pd_obj_exp["exp_" + output]
+        tab = pd.concat([result,expected], axis=1)
+        print(" ")
+        print(tabulate(tab, headers='keys', tablefmt='fancy_grid'))
+        npt.assert_array_almost_equal(result, expected, 4, '', True)
+
+    def blackbox_method_str(self, output):
+        result = terrplant_calc.pd_obj_out["out_" + output]
+        expected = terrplant_calc.pd_obj_exp["exp_" + output]
+        tab = pd.concat([result,expected], axis=1)
+        print(" ")
+        print(tabulate(tab, headers='keys', tablefmt='fancy_grid'))
+        npt.assert_array_equal(result, expected)
+
+# unittest will
+# 1) call the setup method,
+# 2) then call every method starting with "test",
+# 3) then the teardown method
+if __name__ == '__main__':
+    unittest.main()
---
+++ REST_UBER/terrplant_rest/test_terrplant_unittest.py
@@ -0,0 +1,284 @@
+import unittest
+import terrplant_model_rest as terrplant_model
+import pandas as pd
+import numpy.testing as npt
+# the following works when running test script in parent directory as package:
+# python -m tests.test_terrplant_unittest
+# the following works for running as nosetests from parent directory:
+from .. import terrplant_model_rest as terrplant_model
+
+# load transposed qaqc data for inputs and expected outputs
+# csv_transpose_path_in = "./terrplant_qaqc_in_transpose.csv"
+# pd_obj_inputs = pd.read_csv(csv_transpose_path_in, index_col=0, engine='python')
+# print(pd_obj_inputs)
+# csv_transpose_path_exp = "./terrplant_qaqc_exp_transpose.csv"
+# pd_obj_exp_out = pd.read_csv(csv_transpose_path_exp, index_col=0, engine='python')
+# print(pd_obj_exp_out)
+
+# create empty pandas dataframes to create empty terrplant object
+df_empty = pd.DataFrame()
+terrplant_empty = terrplant_model.terrplant("empty", df_empty, df_empty)
+
+test = {}
+
+
+class TestTerrplant(unittest.TestCase):
+    def setup(self):
+        pass
+        # setup the test as needed
+        # e.g. pandas to open sip qaqc csv
+        #  Read qaqc csv and create pandas DataFrames for inputs and expected outputs
+
+    def teardown(self):
+        pass
+        # teardown called after each test
+        # e.g. maybe write test results to some text file
+
+# each of these functions are queued by "run_methods" and have outputs defined as properties in the terrplant qaqc csv
+    def test_rundry(self):
+        """
+        unittest for function terrplant.rundry
+        """
+        #(self.application_rate/self.incorporation_depth) * self.runoff_fraction
+        try:
+            terrplant_empty.application_rate = pd.Series([10.], dtype='int')
+            terrplant_empty.incorporation_depth = pd.Series([2.], dtype='int')
+            terrplant_empty.runoff_fraction = pd.Series([.1], dtype='float')
+            result = terrplant_empty.rundry()
+            npt.assert_array_almost_equal(result, 0.5, '', True)
+        finally:
+            pass
+        return
+
+    def test_runsemi(self):
+        """
+        unittest for function terrplant.runsemi
+        """
+        #self.out_runsemi = (self.application_rate/self.incorporation_depth) * self.runoff_fraction * 10
+        try:
+            terrplant_empty.application_rate = pd.Series([10.], dtype='int')
+            terrplant_empty.incorporation_depth = pd.Series([2.], dtype='int')
+            terrplant_empty.runoff_fraction = pd.Series([.1], dtype='float')
+            result = terrplant_empty.runsemi()
+            npt.assert_array_almost_equal(result,5, '', True)
+        finally:
+            pass
+        return
+
+    def test_spray(self):
+        """
+        unittest for function terrplant.spray
+        """
+        #self.out_spray = self.application_rate * self.drift_fraction
+        try:
+            terrplant_empty.application_rate = pd.Series([10.], dtype='int')
+            terrplant_empty.drift_fraction = pd.Series([0.5], dtype='float')
+            result = terrplant_empty.spray()
+            npt.assert_array_almost_equal(result, 5, '', True)
+        finally:
+            pass
+        return
+
+    def test_totaldry(self):
+        """
+        unittest for function terrplant.totaldry
+        """
+        #self.out_totaldry = self.out_rundry + self.out_spray
+        try:
+            terrplant_empty.rundry = pd.Series([0.5], dtype='float')
+            terrplant_empty.spray = pd.Series([5.], dtype='int')
+            result = terrplant_empty.totaldry()
+            npt.assert_array_almost_equal(result, 5.5, '', True)
+        finally:
+            pass
+        return
+
+    def test_totalsemi(self):
+        """
+        unittest for function terrplant.totalsemi
+        """
+        #self.out_totalsemi = self.out_runsemi + self.out_spray
+        try:
+            terrplant_empty.out_runsemi = pd.Series([5.], dtype='int')
+            terrplant_empty.out_spray = pd.Series([5.], dtype='int')
+            result = terrplant_empty.totalsemi()
+            npt.assert_array_almost_equal(result, 10, '', True)
+        finally:
+            pass
+        return
+
+    def test_nms_rq_dry(self):
+        """
+        unittest for function terrplant.nms_rq_dry
+        """
+        #self.out_nms_rq_dry = self.out_totaldry/self.ec25_nonlisted_seedling_emergence_monocot
+        try:
+            terrplant_empty.out_totaldry = pd.Series([5.5], dtype='float')
+            terrplant_empty.ec25_nonlisted_seedling_emergence_monocot = pd.Series([0.05], dtype='float')
+            result = terrplant_empty.out_nms_rq_dry()
+            npt.assert_array_almost_equal(result, 110, '', True)
+        finally:
+            pass
+        return
+
+    def test_nms_rq_semi(self):
+        """
+        unittest for function terrplant.nms_rq_semi
+        """
+        #self.out_nms_rq_semi = self.out_totalsemi/self.ec25_nonlisted_seedling_emergence_monocot
+        try:
+            terrplant_empty.out_totalsemi = pd.Series([10.], dtype='int')
+            terrplant_empty.ec25_nonlisted_seedling_emergence_monocot = pd.Series([0.05], dtype='float')
+            result = terrplant_empty.out_nms_rq_semi()
+            npt.assert_array_almost_equal(result, 200, '', True)
+        finally:
+            pass
+        return
+
+    def test_nms_rq_spray(self):
+        """
+        unittest for function terrplant.nms_rq_spray
+        """
+        #self.out_nms_rq_spray = self.out_spray/self.ec25_nonlisted_seedling_emergence_monocot
+        try:
+            terrplant_empty.out_spray = pd.Series([5.], dtype='int')
+            terrplant_empty.ec25_nonlisted_seedling_emergence_monocot = pd.Series([0.05], dtype='float')
+            result = terrplant_empty.out_nms_rq_spray()
+            npt.assert_array_almost_equal(result, 100, '', True)
+        finally:
+            pass
+        return
+
+    def test_lms_rq_dry(self):
+        """
+        unittest for function terrplant.lms_rq_dry
+        """
+        #self.out_lms_rq_dry = self.out_totaldry/self.ec25_nonlisted_seedling_emergence_dicot
+        try:
+            terrplant_empty.out_totaldry = pd.Series([5.5], dtype='float')
+            terrplant_empty.ec25_nonlisted_seedling_emergence_dicot = pd.Series([0.01], dtype='float')
+            result = terrplant_empty.out_lms_rq_dry()
+            npt.assert_array_almost_equal(result, 550, '', True)
+        finally:
+            pass
+        return
+
+    def test_lms_rq_semi(self):
+        """
+        unittest for function terrplant.lms_rq_semi
+        """
+        #self.out_lms_rq_semi = self.out_totalsemi/self.ec25_nonlisted_seedling_emergence_dicot
+        try:
+            terrplant_empty.out_totalsemi = pd.Series([10.], dtype='int')
+            terrplant_empty.ec25_nonlisted_seedling_emergence_dicot = pd.Series([0.01], dtype='float')
+            result = terrplant_empty.out_lms_rq_semi()
+            npt.assert_array_almost_equal(result, 1000, '', True)
+        finally:
+            pass
+        return
+
+    def test_lms_rq_spray(self):
+        """
+        unittest for function terrplant.lms_rq_spray
+        """
+        #self.out_lms_rq_spray = self.out_spray/self.ec25_nonlisted_seedling_emergence_dicot
+        try:
+            terrplant_empty.out_spray = pd.Series([5.], dtype='int')
+            terrplant_empty.ec25_nonlisted_seedling_emergence_dicot = pd.Series([0.01], dtype='float')
+            result = terrplant_empty.out_lms_rq_spray()
+            npt.assert_array_almost_equal(result, 500, '', True)
+        finally:
+            pass
+        return
+
+    def test_nds_rq_dry(self):
+        """
+        unittest for function terrplant.nds_rq_dry
+        """
+        #self.out_nds_rq_dry = self.out_totaldry/self.noaec_listed_seedling_emergence_monocot
+        try:
+            terrplant_empty.out_totaldry = pd.Series([5.5], dtype='float')
+            terrplant_empty.noaec_listed_seedling_emergence_monocot = pd.Series([0.02], dtype='float')
+            result = terrplant_empty.out_nds_rq_dry()
+            npt.assert_array_almost_equal(result, 275, '', True)
+        finally:
+            pass
+        return
+
+    def test_nds_rq_semi(self):
+        """
+        unittest for function terrplant.nds_rq_semi
+        """
+        #self.out_nds_rq_semi = self.out_totalsemi/self.noaec_listed_seedling_emergence_monocot
+        try:
+            terrplant_empty.out_totalsemi = pd.Series([10.], dtype='int')
+            terrplant_empty.noaec_listed_seedling_emergence_monocot = pd.Series([0.02], dtype='float')
+            result = terrplant_empty.out_nds_rq_semi()
+            npt.assert_array_almost_equal(result, 500, '', True)
+        finally:
+            pass
+        return
+
+    def test_nds_rq_spray(self):
+        """
+        unittest for function terrplant.nds_rq_spray
+        """
+        #self.out_nds_rq_spray = self.out_spray/self.noaec_listed_seedling_emergence_monocot
+        try:
+            terrplant_empty.out_spray = pd.Series([5.], dtype='int')
+            terrplant_empty.noaec_listed_seedling_emergence_monocot = pd.Series([0.02], dtype='float')
+            result = terrplant_empty.out_nds_rq_spray()
+            npt.assert_array_almost_equal(result, 250, '', True)
+        finally:
+            pass
+        return
+
+    def test_lds_rq_dry(self):
+        """
+        unittest for function terrplant.lds_rq_dry
+        """
+        #self.out_lds_rq_dry = self.out_totaldry/self.noaec_listed_seedling_emergence_dicot
+        try:
+            terrplant_empty.out_totaldry = pd.Series([5.5], dtype='float')
+            terrplant_empty.noaec_listed_seedling_emergence_dicot = pd.Series([0.1], dtype='float')
+            result = terrplant_empty.out_lds_rq_dry()
+            npt.assert_array_almost_equal(result, 55, '', True)
+        finally:
+            pass
+        return
+
+    def test_lds_rq_semi(self):
+        """
+        unittest for function terrplant.lds_rq_semi
+        """
+        #self.out_lds_rq_semi = self.out_totalsemi/self.noaec_listed_seedling_emergence_dicot
+        try:
+            terrplant_empty.out_totalsemi = pd.Series([10.], dtype='int')
+            terrplant_empty.noaec_listed_seedling_emergence_dicot = pd.Series([0.1], dtype='float')
+            result = terrplant_empty.out_lds_rq_semi()
+            npt.assert_array_almost_equal(result, 100, '', True)
+        finally:
+            pass
+        return
+
+    def test_lds_rq_spray(self):
+        """
+        unittest for function terrplant.lds_rq_spray
+        """
+        #self.out_lds_rq_spray = self.out_spray/self.noaec_listed_seedling_emergence_dicot
+        try:
+            terrplant_empty.out_spray = pd.Series([5.], dtype='int')
+            terrplant_empty.noaec_listed_seedling_emergence_dicot = pd.Series([0.1], dtype='float')
+            result = terrplant_empty.out_lds_rq_spray()
+            npt.assert_array_almost_equal(result, 50, '', True)
+        finally:
+            pass
+        return
+
+
+# unittest will
+# 1) call the setup method,
+# 2) then call every method starting with "test",
+# 3) then the teardown method
+if __name__ == '__main__':
+    unittest.main()
