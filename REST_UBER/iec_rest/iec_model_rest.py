ee551225bfaa5105495e6a68fe315aa8e7306ac8
Carmen Kuan <ashleyc.kuan@gmail.com>
2015-10-19 14:04:50 -0400

    vectorize iec

--- REST_UBER/iec_rest/iec_model_rest.py
+++ REST_UBER/iec_rest/iec_model_rest.py
@@ -1,18 +1,63 @@
+# -*- coding: utf-8 -*-
 import math
+import pandas as pd
 
 class iec(object):
-    def __init__(self, dose_response, LC50, threshold):
-        self.dose_response = dose_response
-        self.LC50 = LC50
-        self.threshold = threshold
+    def __init__(self, run_type, pd_obj, pd_obj_exp):
+        # run_type can be single, batch or qaqc
+        # 0 to run calculation, else it wont
+        self.run_type = run_type
+        self.pd_obj = pd_obj
+        self.pd_obj_exp = pd_obj_exp
+        # Execute model methods if requested
+        if self.run_type != "empty":
+            self.execute_model()
 
-        #Result variables
-        self.z_score_f_out = -1
-        self.F8_f_out = -1
-        self.chance_f_out = -1
+    def execute_model(self):
+        self.populate_input_properties()
+        self.create_output_properties()
         self.run_methods()
+        self.create_output_dataframe()
+        self.json = self.json(self.pd_obj, self.pd_obj_out, self.pd_obj_exp)
 
+    def json(self, pd_obj, pd_obj_out, pd_obj_exp):
+        """
+            Convert DataFrames to JSON, returning a tuple
+            of JSON strings (inputs, outputs, exp_out)
+        """
 
+        pd_obj_json = pd_obj.to_json()
+        pd_obj_out_json = pd_obj_out.to_json()
+        try:
+            pd_obj_exp_json = pd_obj_exp.to_json()
+        except:
+            pd_obj_exp_json = "{}"
+        # Callable from Bottle that returns JSON
+        return pd_obj_json, pd_obj_out_json, pd_obj_exp_json
+
+    def populate_input_properties(self):
+        # Inputs: Assign object attribute variables from the input Pandas DataFrame
+        self.dose_response = self.pd_obj['dose_response']
+        self.LC50 = self.pd_obj['LC50']
+        self.threshold = self.pd_obj['threshold']
+
+    def create_output_dataframe(self):
+    # Create DataFrame containing output value Series
+        pdj_obj_out = pd.DataFrame({
+            'z_score_f_out': self.z_score_f_out,
+            'F8_f_out': self.F8_f_out,
+            'chance_f_out': self.chance_f_out
+        })
+        # create pandas properties for acceptance testing
+        self.pd_obj_out = pdj_obj_out
+
+    def create_output_properties(self):
+        # Outputs: Assign object attribute variables to Pandas Series
+        self.z_score_f_out = pd.Series(name='z_score_f_out')
+        self.F8_f_out = pd.Series(name='F8_f_out')
+        self.chance_f_out = pd.Series(name='chance_f_out')
+
+    # begin model methods
     def run_methods(self):
         try:
             self.z_score_f()
