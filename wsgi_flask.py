8086578fb2af68b8bf58efdc8e919f470b08ac27
Jon Flash <jflaisha@ord-uber-vm005.fedcloud.cgipdc.cginet>
2016-07-05 14:11:48 -0700

    Updated WSGI to Flask

--- wsgi_flask.py
+++ wsgi_flask.py
@@ -1,17 +1,16 @@
-import bottle, os, sys
+import os
+import sys
+
 
 # Change working directory so relative paths (and template lookup) work again
 os.chdir(os.path.dirname(__file__))
 
-# Add Django project & parent directory to Python PATH
+# Add Flask app & parent directory to Python PATH
 sys.path.insert(0, '/var/www/ubertool/ubertool_ecorest/REST_UBER')
 sys.path.insert(0, '/var/www/ubertool/ubertool_ecorest')
 # sys.path.insert(0, '/var/www/ubertool')
 
 # ... build or import your bottle application here ...
-# import REST_UBER.bottle_ec2
-import REST_UBER.bottle_cgi
+import flask_cgi
 
-# Do NOT use bottle.run() with mod_wsgi/uwsgi
-
-application = bottle.default_app()
+application = flask_cgi.app
