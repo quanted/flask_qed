be7090f30d9d167bec46262d2de035b2b63702b7
Tom Purucker <purucker.tom@gmail.com>
2016-12-22 22:04:04 +0000

    beerex w carmen

--- REST_UBER/rest_model_caller.py
+++ REST_UBER/rest_model_caller.py
@@ -1,14 +1,25 @@
+import logging
 import pandas as pd
 
 
 def model_run(model, jobId, inputs, module):
 
+    boolog = True
+    if boolog:
+        print('ubertool_ecorest start ================================')
+        print('REST_UBER.rest_model_caller.model_run')
+        print(module)
+        print('inputs')
+        print(inputs)
     pd_obj = pd.DataFrame.from_dict(inputs, dtype='float64')
     model_class = getattr(module, model.capitalize())
     model_obj = model_class(pd_obj, None)
     model_obj.execute_model()
     inputs_json, outputs_json, exp_out_json = model_obj.get_dict_rep()
-
+    if boolog:
+        print('outputs')
+        print(outputs_json)
+        print('ubertool_ecorest stop ================================')
     return {
         'user_id': 'admin',
         'inputs': inputs_json,
