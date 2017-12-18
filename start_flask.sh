a8ef3d7885381715eccc89e60f39e0ef3b14bf0e
Deron <dbsmith@uga.edu>
2017-12-18 16:29:59 -0600

    switched order of commands

--- start_flask.sh
+++ start_flask.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-celery worker -A tasks -c 1 -Q sam --loglevel=DEBUG -n sam_worker
-
 exec uwsgi /etc/uwsgi/uwsgi.ini
+
+celery worker -A tasks -c 1 -Q sam --loglevel=DEBUG -n sam_worker
