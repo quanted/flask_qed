d6f2055ddc3f6a9be823bec1dd1c193d509334aa
dbsmith88 <dbsmith@uga.edu>
2018-03-30 08:25:23 -0400

    logging added to flask apps

--- flask_cgi.py
+++ flask_cgi.py
@@ -1,13 +1,16 @@
 from werkzeug.wsgi import DispatcherMiddleware
 from werkzeug.serving import run_simple
+import logging
 
 import flask_pram as pram
 import flask_hms as hms
 
-
+logging.debug("flask_cgi started: live flask apps")
 app = DispatcherMiddleware(pram.app, {
     '/hms': hms.app,
 })
+logging.debug("flask_pram")
+logging.debug("flask_hms")
 
 if __name__ == "__main__":
     run_simple('localhost', 7777, app, use_reloader=True, use_debugger=True, use_evalex=True)
--- flask_hms.py
+++ flask_hms.py
@@ -1,14 +1,13 @@
-from flask import Flask, Response
-# from flask_cors import CORS
+from flask import Flask
 from flask_restful import Api, Resource
 import os
+import logging
 
 # Import modules
 from modules.hms import ncdc_stations
 from modules.hms import percent_area
 
 app = Flask(__name__)
-# CORS(app)
 app.config.update(
     DEBUG=True
 )
@@ -22,16 +21,20 @@ os.environ.update({
 
 
 class StatusTest(Resource):
-   def get(self):
-       return {"status": "GIS_QED up and running."}
+    def get(self):
+        return {"status": "flask_hms up and running."}
 
 
-print('Starting hms flask...')
+base_url = "https://localhost:7777/hms"
+logging.debug("flask_hms started: live endpoints")
+logging.debug(base_url + "/gis")
 api.add_resource(StatusTest, '/gis/test/')
 
 # HMS endpoints
 # TODO: add endpoint for get after converting post endpoint to celery function
+logging.debug(base_url + "/gis/ncdc/stations/")
 api.add_resource(ncdc_stations.HMSNcdcStations, '/gis/ncdc/stations/')
+logging.debug(base_url + "/gis/percentage/")
 api.add_resource(percent_area.getPercentArea, '/gis/percentage/')
 
 
