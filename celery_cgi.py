c2c0f8399b88dbea90d81a8589cb447d0ed84703
Tom Purucker <purucker.tom@epa.gov>
2018-04-11 14:34:55 -0700

    fixed import for celery

--- celery_cgi.py
+++ celery_cgi.py
@@ -2,12 +2,12 @@ from celery import Celery
 
 
 celery_tasks = [
-    'flask_qed.hms_flask.modules.hms_controller',
-    'flask_qed.pram_flask.tasks'
+    'hms_flask.modules.hms_controller',
+    'pram_flask.tasks'
 ]
 
 # celery = Celery('flask_qed', broker='redis://localhost:6379/0', backend='redis://localhost:6379/0', include=celery_tasks)
-celery = Celery('flask_qed', broker='redis://redis:6379/0', backend='redis://redis:6379/0', include=['flask_qed.hms_flask'])
+celery = Celery('flask_qed', broker='redis://redis:6379/0', backend='redis://redis:6379/0', include=celery_tasks)
 
 celery.conf.update(
     CELERY_ACCEPT_CONTENT=['json'],
