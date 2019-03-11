e7af1e49f9f6996b9a25f8cd10e889ff9d554351
dbsmith88 <dbsmith@uga.edu>
2019-03-11 10:23:10 -0500

    commented out unit testing

--- start_flask.sh
+++ start_flask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #sh ./run_pytests_docker.sh > run_pytests_docker_output.txt
-sh ./run_pytests.sh > run_pytests_docker_output.txt
+#sh ./run_pytests.sh > run_pytests_docker_output.txt
 rm -rf /src/collected_static/pram_qaqc_reports 
 cp -r /src/pram_flask/pram_qaqc_reports /src/collected_static 
 exec uwsgi /etc/uwsgi/uwsgi.ini
