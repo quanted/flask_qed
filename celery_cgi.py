c8797db1981fbf813b58d75c345c9a74563052b4
dbsmith88 <dbsmith@uga.edu>
2019-03-15 11:19:56 -0500

    bug fixes and updates for temp config for kube

--- celery_cgi.py
+++ celery_cgi.py
@@ -14,8 +14,14 @@ celery_tasks = [
     'pram_flask.tasks'
 ]
 
-redis = 'redis://' + redis_server + ':' + redis_port + '/0'
-logging.info("Celery connecting to redis server: " + redis)
+if os.environ.get('DOCKER_HOSTNAME'):
+    if "KUBERNETES" in os.environ.get('DOCKER_HOSTNAME'):
+        redis = 'http://' + redis_server + ':' + redis_port
+    else:
+        redis = 'redis://' + redis_server + ':' + redis_port + '/0'
+else:
+    redis = 'redis://' + redis_server + ':' + redis_port + '/0'
+print("Celery connecting to redis server: " + redis)
 
 celery = Celery('flask_qed', broker=redis, backend=redis, include=celery_tasks)
 
