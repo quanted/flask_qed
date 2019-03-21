4726b4015384a5ca30204553ade2c348efa8a4e1
Deron Smith <dsmith@Derons-MacBook-Pro.local>
2019-03-21 10:16:54 -0500

    updated start uwsgi command

--- start_flask.sh
+++ start_flask.sh
@@ -4,6 +4,6 @@
 #sh ./run_pytests.sh > run_pytests_docker_output.txt
 rm -rf /src/collected_static/pram_qaqc_reports 
 cp -r /src/pram_flask/pram_qaqc_reports /src/collected_static 
-exec uwsgi /etc/uwsgi/uwsgi.ini
+exec uwsgi --ini /etc/uwsgi/uwsgi.ini
 
 # celery worker -A tasks -c 1 -Q sam --loglevel=DEBUG -n sam_worker
