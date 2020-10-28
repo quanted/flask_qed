c08bc9057a92f54d42c0c5ddb5ddfae83f01d71f
triphook <hook.james@epa.gov>
2020-10-28 14:35:32 -0400

    silencing warnings

--- wsgi_flask.py
+++ wsgi_flask.py
@@ -1,6 +1,8 @@
 import os
 import sys
-
+import warnings
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 # Change working directory so relative paths (and template lookup) work again
 os.chdir(os.path.dirname(__file__))
